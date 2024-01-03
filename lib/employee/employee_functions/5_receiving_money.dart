5738594be965d7c1699568d42cf740e70a4e76c0
Mohammad Zaied <mohammad.zaied.8@gmail.com>
2024-01-03 16:29:44 +0200

    11

--- lib/employee/employee_functions/5_receiving_money.dart
+++ lib/employee/employee_functions/5_receiving_money.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_1/employee/main_page_employee.dart';
 import 'package:flutter_application_1/style/common/theme_h.dart';
 import 'package:flutter_typeahead/flutter_typeahead.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class receiving_money extends StatefulWidget {
   @override
@@ -9,56 +12,74 @@ class receiving_money extends StatefulWidget {
 }
 
 class _receiving_moneyState extends State<receiving_money> {
-  late List<driver> persons;
+  List<driver> drivers = [];
+  List<dynamic> new_drivers = [];
+
   late driver selected_driver;
   TextEditingController controller = new TextEditingController();
 
-  void loadData() async {
-    persons = [
-      driver(
-        name: 'mohammad',
-        img: "assets/f3.png",
-        num_pkg_deliver: 10,
-        num_pkg_receive: 12,
-        total_delivery: 1500,
-        total_purchase: 300,
-      ),
-      driver(
-        name: 'rami',
-        img: "assets/add-friend.png",
-        num_pkg_deliver: 11,
-        num_pkg_receive: 2,
-        total_delivery: 3300,
-        total_purchase: 200,
-      ),
-      driver(
-        name: 'zain',
-        img: "assets/driver.png",
-        num_pkg_deliver: 1,
-        num_pkg_receive: 1,
-        total_delivery: 100,
-        total_purchase: 100,
-      ),
-      driver(
-        name: 'ahmad',
-        img: "",
-        num_pkg_deliver: 30,
-        num_pkg_receive: 15,
-        total_delivery: 4000,
-        total_purchase: 200,
-      ),
-    ];
+  String username = GetStorage().read("userName");
+  String password = GetStorage().read("password");
 
-    for (int i = 0; i < 10; i++) {
-      persons.add(driver(
-        name: 'rida',
-        img: "assets/driver.png",
-        num_pkg_deliver: 40,
-        num_pkg_receive: 15,
-        total_delivery: 5000,
-        total_purchase: 700,
+  Future post_checkout() async {
+    var url = urlStarter + "/employee/receiveDriverBalance";
+    var responce = await http.post(Uri.parse(url),
+        body: jsonEncode({
+          "employeeUserName": username,
+          "employeePassword": password,
+          "driverUsername": selected_driver.username,
+          "deliverdNumber": selected_driver.num_pkg_deliver,
+          "receivedNumber": selected_driver.num_pkg_receive,
+          "totalBalance": selected_driver.total_delivery
+        }),
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        });
+    if (responce.statusCode == 200) {
+      //driver dr = selected_driver;
+      setState(() {
+        fetchData_drivers();
+        // controller.text = dr.name;
+        // selected_driver = dr;
+      });
+    }
+  }
+
+  Future<void> fetchData_drivers() async {
+    var url = urlStarter + "/employee/getDriversBalance";
+    print(url);
+    final response = await http
+        .get(Uri.parse(url), headers: {'ngrok-skip-browser-warning': 'true'});
+    if (response.statusCode == 200) {
+      var data = jsonDecode(response.body);
+      new_drivers = data;
+      print(new_drivers);
+      setState(() {
+        drivers = loadData();
+        selected_driver = drivers.first;
+        controller.text = selected_driver.name;
+      });
+    } else {
+      print('new_orders error');
+      throw Exception('Failed to load data');
+    }
+  }
+
+  List<driver> loadData() {
+    List<driver> d = [];
+
+    for (int i = 0; i < new_drivers.length; i++) {
+      d.add(driver(
+        name: new_drivers[i]['name'],
+        username: new_drivers[i]['username'],
+        img: new_drivers[i]['img'],
+        num_pkg_deliver: new_drivers[i]['deliverdNumber'],
+        num_pkg_receive: new_drivers[i]['receivedNumber'],
+        total_delivery: new_drivers[i]['totalBalance'],
+        total_purchase: 0,
       ));
     }
+    return d;
   }
 
   @override
@@ -66,9 +87,16 @@ class _receiving_moneyState extends State<receiving_money> {
     setState(() {
       TabController_.index = 4;
     });
-    loadData();
-    selected_driver = persons[0];
-    controller.text = selected_driver.name;
+    selected_driver = driver(
+      name: '',
+      username: '',
+      img: '',
+      num_pkg_deliver: 0,
+      num_pkg_receive: 0,
+      total_delivery: 0,
+      total_purchase: 0,
+    );
+    fetchData_drivers();
     super.initState();
   }
 
@@ -121,7 +149,7 @@ class _receiving_moneyState extends State<receiving_money> {
                         ),
                         suggestionsCallback: (pattern) {
                           print('object');
-                          return persons
+                          return drivers
                               .where((driver) => driver.name
                                   .toLowerCase()
                                   .startsWith(pattern.toLowerCase()))
@@ -133,7 +161,11 @@ class _receiving_moneyState extends State<receiving_money> {
                               backgroundColor: primarycolor,
                               backgroundImage: suggestion.img == ""
                                   ? null
-                                  : AssetImage("${suggestion.img}"),
+                                  : NetworkImage(suggestion.img,
+                                      scale: 1,
+                                      headers: {
+                                          'ngrok-skip-browser-warning': 'true'
+                                        }),
                               child: suggestion.img == ""
                                   ? Text(
                                       suggestion.name[0]
@@ -144,6 +176,7 @@ class _receiving_moneyState extends State<receiving_money> {
                                   : null,
                             ),
                             title: Text(suggestion.name),
+                            subtitle: Text(suggestion.username),
                           );
                         },
                         onSuggestionSelected: (driver suggestion) {
@@ -178,14 +211,19 @@ class _receiving_moneyState extends State<receiving_money> {
                 children: [
                   Center(
                     child: Container(
-                      height: 180,
-                      width: 180,
+                      height: 140,
+                      width: 140,
                       decoration: BoxDecoration(
-                          shape: BoxShape.circle,
+                          shape: BoxShape.rectangle,
                           //borderRadius: BorderRadius.circular(20),
                           image: DecorationImage(
-                              fit: BoxFit.cover,
-                              image: AssetImage(selected_driver.img))),
+                            fit: BoxFit.cover,
+                            image: NetworkImage(selected_driver.img,
+                                scale: 1,
+                                headers: {
+                                  'ngrok-skip-browser-warning': 'true'
+                                }),
+                          )),
                     ),
                   ),
                   SizedBox(
@@ -197,6 +235,22 @@ class _receiving_moneyState extends State<receiving_money> {
                       children: <InlineSpan>[
                         TextSpan(
                           text: ' ${selected_driver.name}',
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        )
+                      ])),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text.rich(TextSpan(
+                      text: 'Username: ',
+                      style: TextStyle(fontSize: 20, color: Colors.grey),
+                      children: <InlineSpan>[
+                        TextSpan(
+                          text: ' ${selected_driver.username}',
                           style: TextStyle(
                             fontSize: 18,
                             color: Colors.black,
@@ -306,7 +360,54 @@ class _receiving_moneyState extends State<receiving_money> {
                                   color: Colors.white),
                             ),
                           ),
-                          onPressed: () {},
+                          onPressed: () {
+                            showDialog(
+                                context: context,
+                                builder: (context) {
+                                  return AlertDialog(
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(20.0),
+                                    ),
+                                    actions: [
+                                      TextButton(
+                                          onPressed: () {
+                                            post_checkout();
+                                            Navigator.of(context).pop();
+                                          },
+                                          child: Text(
+                                            "Yes",
+                                            style: TextStyle(
+                                                color: Colors.white,
+                                                fontSize: 18),
+                                          )),
+                                      TextButton(
+                                          onPressed: () {
+                                            Navigator.of(context).pop();
+                                          },
+                                          child: Text(
+                                            "Cancel",
+                                            style: TextStyle(
+                                                color: Colors.red,
+                                                fontSize: 18),
+                                          )),
+                                    ],
+                                    title: Text("Chekout"),
+                                    content: Container(
+                                      width: 400,
+                                      child: Text(
+                                        "Are you sure you to chekout the driver's ${selected_driver.name} ?",
+                                        style: TextStyle(
+                                            color: Colors.white, fontSize: 18),
+                                      ),
+                                    ),
+                                    titleTextStyle: TextStyle(
+                                        color: Colors.white, fontSize: 25),
+                                    contentTextStyle: TextStyle(
+                                        color: Colors.white, fontSize: 16),
+                                    backgroundColor: primarycolor,
+                                  );
+                                });
+                          },
                         ),
                       ),
                     ],
@@ -323,6 +424,7 @@ class _receiving_moneyState extends State<receiving_money> {
 
 class driver {
   final String name;
+  final String username;
   final String img;
   final int num_pkg_receive;
   final int num_pkg_deliver;
@@ -331,6 +433,7 @@ class driver {
 
   driver({
     required this.num_pkg_receive,
+    required this.username,
     required this.num_pkg_deliver,
     required this.total_delivery,
     required this.total_purchase,
